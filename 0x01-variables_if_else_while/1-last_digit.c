ba55b6615b022217486999fd08767a656247abf6
Marwa <mm6001@fayoum.edu.eg>
2023-07-14 09:45:09 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,6 @@ int main(void)
 	if (n % 10 < 6 && n % 10 != 0)
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	if (n % 10 == 0)
-	printf("Last digit of %d is 0 and is 0\n", n, 0)
+	printf("Last digit of %d is 0 and is 0\n", n);
 	return (0);
 }
