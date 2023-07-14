fcefe00b69a01bd646b8b3a09e8564707a37a4e9
Marwa <mm6001@fayoum.edu.eg>
2023-07-14 12:27:45 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 {
 	int a;
 
-	for (a = 0 ; a <= 9 ; a++)
+	for (a = 0 ; a < 9 ; a++)
 	{
 	putchar('0' + a);
 	putchar(',');
 	putchar(' ');
 	}
-	putchar('\n');
+	putchar('9');
 	return (0);
 }
