37616f05e21986180ad25fcb09c3a11eda3eba17
Marwa <mm6001@fayoum.edu.eg>
2023-07-14 12:25:17 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using for loop to print numbers of base 16
+ *
+ * Return: Always (0)
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+	putchar('0' + a);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
