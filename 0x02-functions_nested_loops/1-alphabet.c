d91ed0e9949f1c88cadf0f3d06a8a2d05febd893
Marwa <mm6001@fayoum.edu.eg>
2023-07-18 16:37:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ *Description: using _putchar function to print _putchar
+ *
+ * Return: Always (0)
+*/
+
+int main(void)
+{
+        print_alphabet();
+        return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,15 @@ int _putchar(char c)
         return (write(1, &c, 1));
 }
 
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	_putchar(i);
+	_putchar('\n');
+}
+
 void _putchar1()
 {
 	_putchar('_');
