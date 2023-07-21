1a88b1e7d39eb4b3ef043d5d08d2981c71760e13
Marwa <mm6001@fayoum.edu.eg>
2023-07-20 17:27:36 -0700

    print fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 != 0 && i % 5 != 0)
-			printf("%d ", i);
+			printf("%d", i);
 		else if (i % 3 == 0 && i % 5 != 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0 && i % 3 != 0)
-			printf("Buzz ");
-		else
-			printf("FizzBuzz ");
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
 		if (i != 100)
 			printf(" ");
 		else
